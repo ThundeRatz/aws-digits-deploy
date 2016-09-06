61489d73fb2c66750ab1a0db28afa092c9972c37
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2016-09-06 16:13:05 -0300

    Automate generation of datasets

--- kitti_formatter/formatter.py
+++ kitti_formatter/formatter.py
@@ -13,17 +13,21 @@ def parse_args():
     parser.add_argument('input', help='Input file with list of xmin ymin xmax ymax')
     parser.add_argument('output', help='Output KITTI formatted file (see https://github.com/' +
                         'NVIDIA/DIGITS/blob/master/digits/extensions/data/objectDetection/README.md and' +
-                        'http://www.cvlibs.net/datasets/kitti/eval_object.php)')
+                        'http://www.cvlibs.net/datasets/kitti/eval_object.php)q')
     return parser.parse_args()
 
 
-def main():
-    args = parse_args()
-    with open(args.input) as input_file, open(args.output, 'w') as output:
+def kitti_format(input_filename, output_filename):
+    with open(input_filename) as input_file, open(output_filename, 'w') as output:
         coords = parse_input(input_file.readlines())
         for left, top, right, bottom in coords:
             output.write('{type} 0 0 0 {left} {top} {right} {bottom} 0 0 0 0 0 0 0 0\n'
                          .format(type='Cone', left=left, top=top, right=right, bottom=bottom))
+
+
+def main():
+    args = parse_args()
+    kitti_format(args.input, args.output)
 
 if __name__ == "__main__":
     main()
---
+++ kitti_formatter/generate_datasets.py
@@ -0,0 +1,60 @@
+import formatter
+import os
+import random
+import shutil
+
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='Directory with images and labels folders')
+    parser.add_argument('output', help='Output directory')
+    return parser.parse_args()
+
+
+def get_file_list(directory):
+    images = {x[:-4] for x in os.listdir(os.path.join(directory, 'images')) if x.endswith('.png')}
+    labels = {x[:-4] for x in os.listdir(os.path.join(directory, 'labels')) if x.endswith('.txt')}
+    empty_labels = set()
+    for label in labels:
+        with open(os.path.join(directory, 'labels', '{}.txt'.format(label))) as f:
+            if len(f.read()) < 3:
+                print('Ignoring label file {} with no detections'.format(label))
+                empty_labels.add(label)
+    labels -= empty_labels
+    if images - labels:
+        print('Missing labels: {}'.format(images - labels))
+    if labels - images:
+        print('Missing images: {}'.format(labels - images))
+    return images & labels
+
+
+def create_dir_structure(output):
+    os.makedirs(output, exist_ok=True)
+    for d in 'train', 'val':
+        os.mkdir(os.path.join(output, d))
+        os.mkdir(os.path.join(output, d, 'labels'))
+        os.mkdir(os.path.join(output, d, 'images'))
+
+
+def main(args):
+    files = get_file_list(args.input)
+    create_dir_structure(args.output)
+
+    validation = set(random.sample(files, len(files) // 10))
+    train = files - validation
+
+    for i, f in enumerate(validation):
+        shutil.move(os.path.join(args.input, 'images', '{}.png'.format(f)),
+                    os.path.join(args.output, 'val', 'images', '{:06}.png'.format(i + 1)))
+        formatter.kitti_format(os.path.join(args.input, 'labels', '{}.txt'.format(f)),
+                               os.path.join(args.output, 'val', 'labels', '{:06}.txt'.format(i + 1)))
+
+    for j, f in enumerate(train):
+        shutil.move(os.path.join(args.input, 'images', '{}.png'.format(f)),
+                    os.path.join(args.output, 'train', 'images', '{:06}.png'.format(i + j + 1)))
+        formatter.kitti_format(os.path.join(args.input, 'labels', '{}.txt'.format(f)),
+                               os.path.join(args.output, 'train', 'labels', '{:06}.txt'.format(i + 1)))
+
+if __name__ == '__main__':
+    main(parse_args())
