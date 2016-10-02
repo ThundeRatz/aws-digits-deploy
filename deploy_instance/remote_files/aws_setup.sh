74d1e5871a907ca7060d34f6bd352b16a21ba489
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2016-10-02 18:59:42 -0300

    Set docker storage engine to overlay2, start (nvidia-)docker after installation

--- deploy_instance/remote_files/aws_setup.sh
+++ deploy_instance/remote_files/aws_setup.sh
@@ -20,12 +20,15 @@ sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt-get update
 apt_install docker-engine
+echo 'DOCKER_OPTS="--storage-driver=overlay2"' | sudo tee /etc/default/docker
 sudo systemctl enable docker.service
+sudo systemctl start docker.service
 
 # NVIDIA-docker
 wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.0-rc.3/nvidia-docker_1.0.0.rc.3-1_amd64.deb
 sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
 sudo systemctl enable nvidia-docker.service
+sudo systemctl start nvidia-docker.service
 
 # EBS setup (1)
 sudo mkfs -t ext4 /dev/xvdb
